e0141b3cb8cc49178aaa6cef4cb38dc23a9dc74d
EC2 Default User <ec2-user@ip-172-31-7-51.ap-northeast-2.compute.internal>
2018-03-15 15:03:51 +0000

    reset

--- index.js
+++ index.js
@@ -2,20 +2,23 @@
     var express = require("express");
     var path = require("path");
     var app = express();
-    
+    var server = require("http").createServer(app)
+
     const appList = [
-        {name: "dcextract", foldername: "dc-extraction"},
-        {name: "sketch", foldername: "project-sketch"},
+        {name: "dc", foldername: "dc-extraction"},
+        {name: "sketch", foldername: "open-sketch"},
+	{name: "uncytick", foldername: "uncytick"},
     ]
     
     appList.forEach(({name, foldername}) => {
         const serverPath = path.resolve("../",foldername,"server");
         app.use(`/${name}`, require(serverPath) );
     });
-    
+
     
     
     app.listen(8080,(req,res) => {
-        console.log("Express server has started on port 8080");
+        console.log("Express server has started on port 8000");
     });
     
+
