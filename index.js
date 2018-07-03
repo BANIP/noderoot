285a18341f1a162c5e9b9266ce78ae5a60f8efe2
EC2 Default User <rnaos90@naver.com>
2018-07-03 06:47:00 +0000

    to byongshintv

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 
     var express = require("express");
     var path = require("path");
+    var fs = require("fs");
     var app = express();
-    const server = require("http").createServer(app);
 
-    const appList = [
-        {name: "uncytick", foldername: "uncyTick"},
+    const options = {
+        key: fs.readFileSync("/etc/letsencrypt/live/byongshine.tk/privkey.pem"),
+        cert: fs.readFileSync("/etc/letsencrypt/live/byongshine.tk/fullchain.pem")
+    };
+
+   const server = require("https").createServer(options,app);
+  // server.http = require("http").createServer(app);
+   const appList = [
+        {name: "uncytick", foldername: "uncytick"},
     ]
     
     appList.forEach(({name, foldername}) => {
         const serverPath = path.resolve("../",foldername,"server");
-        app.use(`/${name}`, require(serverPath) );
+        app.use(`/${name}`, require(serverPath)(server) );
     });
 
-    server.listen(8080,() => console.log("server start"))
+   // server.listen(80,() => console.log("http server start"));
+    server.listen(443,() => console.log("https server start"))
